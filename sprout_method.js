3f73bcb62e99ff9a41a469228816fb0051516a60
Oleg Blokhin <blohin.o.d@gmail.com>
2016-10-03 17:36:14 +0300

    sprout

--- sprout_method.js
+++ sprout_method.js
@@ -1,10 +1,25 @@
-function handleTransaction(entries){
+function uniqueEntries(entries) {
+    var entriesToAdd = [];
     var transactionBundle = getTransactionBundle();
 
-    for (var i = 0; i < entries.length; i++) {
+    for (var i = 0; i < entries.length; i++){
         var entry = entries[i];
+        if (transactionBundle.getListManager().hasEntry(entry)) {
+            var entry = entries[i];
+            entriesToAdd.add(entry);
+        }
+    }
+
+    return entriesToAdd;
+}
+
+function handleTransaction(entries){
+    var transactionBundle = getTransactionBundle();
+    var entriesToAdd = uniqueEntries(entries);
+    for (var i = 0; i < entriesToAdd.length; i++) {
+        var entry = entriesToAdd[i];
         postData(entry);
     }
 
-    transactionBundle.getListManager().add(entry);
+    transactionBundle.getListManager().add(entriesToAdd);
 }
